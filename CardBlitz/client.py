ba633ec80d580ca3b61e1fbd74a707ad34d667ff
FrozenBurger <jenova195@gmail.com>
2020-07-03 18:18:03 +0900

    implementing drag and drop

--- CardBlitz/client.py
+++ CardBlitz/client.py
@@ -1,7 +1,6 @@
 import os
 
 import pygame
-from pygame import event
 import CardBlitz.static_values as val
 import pandas as pd
 
@@ -12,13 +11,14 @@ pygame.display.set_caption("Card Blitz")
 # in-game resource tracking
 map = []
 card_database = []
-objects = {}
+objects = []
+
 
 # --------------------------------------------------------------------------------
 
 # properties of a card
-class card():
-    def __init__(self, id, name, atk, range, hp, spd, image, x, y):
+class Card():
+    def __init__(self, id, name, atk, range, hp, spd, image):
         self.id = id
         self.name = name
         self.atk = atk
@@ -26,24 +26,33 @@ class card():
         self.hp = hp
         self.spd = spd
         self.image = image
-        self.x = x # in-class coordinates store single digit numbers
-        self.y = y # in-class coordinates store single digit numbers
+
+# properties of an object
+class Object():
+    def __init__(self, card, xpos, ypos, owner):
+        self.card = card
+        self.xpos = xpos
+        self.ypos = ypos
+        self.drag = False
+        self.owner = owner
+        self.rect = pygame.rect.Rect(xpos, ypos, xpos+val.square_size, ypos+val.square_size)
+
+    # visualize map
 
 
-
-# visualize map
 def init_map():
     window.fill(val.color_black)
     for i in range(7):
         map.append([])
         for j in range(7):
-            map[i].append([i*val.square_size, j*val.square_size])
+            map[i].append([i * val.square_size, j * val.square_size])
             pygame.draw.rect(window, val.color_white,
-                (val.map_offset+i*(val.square_size+val.seperator_size),
-                 val.map_offset+j*(val.square_size+val.seperator_size),
-                 val.square_size,
-                 val.square_size)
-            )
+                             (val.map_offset + i * (val.square_size + val.seperator_size),
+                              val.map_offset + j * (val.square_size + val.seperator_size),
+                              val.square_size,
+                              val.square_size)
+                             )
+
 
 # load data
 def init_database():
@@ -53,49 +62,52 @@ def init_database():
         img = pygame.image.load(os.path.join(os.getcwd(), 'data', 'img', image_file)).convert()
         image_list.append(img)
     for index, row in db.iterrows():
-        new_card = card(index, row['name'], row['attack'], row['range'], row['health'], row['speed'], image_list[index], 0, 0)
+        new_card = Card(index, row['name'], row['attack'], row['range'], row['health'], row['speed'], image_list[index])
         card_database.append(new_card)
 
+
 def init_objects():
-    objects['p1'] = [card_database[0], card_database[1], card_database[2]]
-    objects['p2'] = [card_database[3], card_database[4], card_database[5]]
-    objects['etc'] = []
-    objects['p2'][0].x, objects['p2'][0].y = 0, 0
-    objects['p2'][1].x, objects['p2'][1].y = 3, 0
-    objects['p2'][2].x, objects['p2'][2].y = 6, 0
-    objects['p1'][0].x, objects['p1'][0].y = 0, 6
-    objects['p1'][1].x, objects['p1'][1].y = 3, 6
-    objects['p1'][2].x, objects['p1'][2].y = 6, 6
+    objects.append(Object(card_database[0], 0, 0, 'p2'))
+    objects.append(Object(card_database[1], 3, 0, 'p2'))
+    objects.append(Object(card_database[2], 6, 0, 'p2'))
+    objects.append(Object(card_database[3], 0, 6, 'p1'))
+    objects.append(Object(card_database[4], 3, 6, 'p1'))
+    objects.append(Object(card_database[5], 6, 6, 'p1'))
 
+drag_start_x = 0
+drag_start_y = 0
 def draw_objects():
-    for obj in objects['p1']:
-        window.blit(obj.image, (val.map_offset+obj.x*(val.square_size+val.seperator_size), val.map_offset+obj.y*(val.square_size+val.seperator_size)))
-    for obj in objects['p2']:
-        window.blit(obj.image, (val.map_offset+obj.x*(val.square_size+val.seperator_size), val.map_offset+obj.y*(val.square_size+val.seperator_size)))
+    for obj in objects:
+        window.blit(obj.card.image, (val.map_offset + obj.xpos * (val.square_size + val.seperator_size),
+                                val.map_offset + obj.ypos * (val.square_size + val.seperator_size)))
+        if obj.drag == True:
+            drag_start_x = obj.xpos
+            drag_start_y = obj.ypos
 
-'''
-def drag_controller():
+def drag_controller(event):
     if event.type == pygame.MOUSEBUTTONDOWN:
+        print('clicked!')
         for object in objects:
             if object.rect.collidepoint(event.pos):
-                dragged = draggable
+                object.drag = True
                 break
+        pygame.draw.line(window, val.color_green, event.pos, (drag_start_x, drag_start_y))
     elif event.type == pygame.MOUSEBUTTONUP:
         dragged = None
-        '''
-
 
 
 # def init_etc_objects():
-
 
 
 def main():
     init_map()
     init_database()
     init_objects()
-    while(True):
-        event.get()
+    while (True):
+        for event in pygame.event.get():
+            drag_controller(event)
         draw_objects()
         pygame.display.update()
+
+
 main()
--- CardBlitz/static_values.py
+++ CardBlitz/static_values.py
@@ -2,6 +2,7 @@ window_width = 800
 window_height = 800
 color_black = (0, 0, 0)
 color_white = (255, 255, 255)
+color_green = (0, 255, 0)
 map_offset = 35
 square_size = 100
 seperator_size = 5
